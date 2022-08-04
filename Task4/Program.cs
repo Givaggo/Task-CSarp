e06f9bc71b7e8bfa5e709345f5bb305870fd58d8
Oleg <0944000@gmail.com>
2022-08-04 17:40:18 +0300

    Красивое решение на три входящих числа

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -2,18 +2,25 @@
 int num_1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите значение № 2: ");
 int num_2 = Convert.ToInt32(Console.ReadLine());
-
-
-
-if (num_1 > num_2)
-{
-    Console.WriteLine($"{num_1} больше {num_2}");
-}
-else
-{
-    Console.WriteLine($"{num_2} больше {num_1}");
-}
-if (num_1 == num_2)
-{
-    Console.WriteLine("Значения равны");
-}
+Console.Write("Введите значение № 3: ");
+int num_3 = Convert.ToInt32(Console.ReadLine());
+        
+int max = 0;
+int min = 0;
+ 
+    if (num_1 >= num_2 && num_1 >= num_3)
+        max = num_1;
+    else if (num_2 >= num_1 && num_2 >=num_3 )
+        max = num_2;
+    else if (num_3 >= num_1 && num_3 >= num_2)
+        max = num_3;
+ 
+    if (num_1<= num_2 && num_1 <= num_3)
+        min = num_1;
+    else if (num_2 <= num_1 && num_2 <= num_3)
+        min = num_2;
+    else if (num_3 <= num_1 && num_3 <= num_2)
+        min = num_3;
+ 
+Console.WriteLine("Максимальное число {0} " , max);
+Console.ReadLine();
